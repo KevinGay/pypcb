76a37b9d792a776bda264f10c686f5610c9d746b
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-08-12 10:50:35 -0700

    Adding textDetector scripts

---
+++ textDetector/config.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+caffe_root = '/data0/mahaling/TEXT/CTPN/caffe/'
+
+import numpy as np
+
+class Config:
+    MEAN=np.float32([102.9801, 115.9465, 122.7717])
+    TEST_GPU_ID=0
+    SCALE=600
+    MAX_SCALE=1000
+
+    LINE_MIN_SCORE=0.7
+    TEXT_PROPOSALS_MIN_SCORE=0.7
+    TEXT_PROPOSALS_NMS_THRESH=0.3
+    MAX_HORIZONTAL_GAP=50
+    TEXT_LINE_NMS_THRESH=0.3
+    MIN_NUM_PROPOSALS=2
+    MIN_RATIO=1.2
+    MIN_V_OVERLAPS=0.7
+    MIN_SIZE_SIM=0.7
+    TEXT_PROPOSALS_WIDTH=16
+    NET_FILE="models/deploy.prototxt"
+    MODEL_FILE="models/ctpn_trained_model.caffemodel"
+
+    MIN_CONTOUR_AREA=100
+
+    FONT_MODEL=""
+    FONT_MEANFILE=""
+    FONT_PROTO=""
+    FONT_LABEL=""
+    FONT_DIMS=32
+    FONT_LBLFILE=""
+
+def init():
+    sys.path.insert(0, caffe_root + 'python')
+init()
